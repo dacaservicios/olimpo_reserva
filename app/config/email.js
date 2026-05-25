ee2eeb03d90411c86e5767af458b80b969e5dc2a
Diego Alarcón <alarconpdiegoa@gmail.com>
2026-05-24 22:45:48 -0500

    se limpio proyecto

--- app/config/email.js
+++ app/config/email.js
@@ -1,109 +1,32 @@
 const nodemailer = require('nodemailer');
 const config = require('./config');
 
-const enviaEmail = async (para,asunto, mensaje, ruta, file)=>{
-
-    /*const transporter = nodemailer.createTransport(smtpTransport({
-        "host": smtp.gmail.com,
-        "port": 465,
-        "secure": true,
-        "debug": true,
-        "auth": {
-            "user" : informes@aynisystem.com, 
-            "pass" : chiatito2020@@
-        },
-        "tls" : {
-           "rejectUnauthorized" : config.REJECTUNAUTHORIZED
-
-        }
-    }));*/
-    /*const transporter = nodemailer.createTransport(smtpTransport({
-        "name": 'zoho',
-        "host": config.HOST_EMAIL,
-        "port": config.PORT_EMAIL2,
-        "secure": config.SECURE2,
-        "debug": config.DEBUG,
-        "auth": {
-            "user" : config.USER_EMAIL, 
-            "pass" : config.PASSWORD_EMAIL
-        },
-        "tls" : {
-           "rejectUnauthorized" : false
-        }
-    }));
-
-    const info = await transporter.sendMail({
-        from: config.CORREO_SERVIDOR, // sender address
-        to: para, // list of receivers
-        bcc : config.EMAIL_HIDDEN,
-        //cc : '',
-        subject: asunto, // Subject line
-        text: mensaje, // plain text body
-        //attachments: archivo,
-        html: mensaje // html body
-    },
-
-    function(error, info){
-    	if(error){
-        	return console.log(error);
-    	}
-    	console.log('Message sent: ' + info.response);
-    });*/
-
+const enviaEmail = async (para, asunto, mensaje, ruta, file) => {
     const transporter = nodemailer.createTransport({
-        "host": config.HOST_EMAIL,
-        "port": config.PORT_EMAIL2,
-        "secure": config.SECURE2,
-        "debug": config.DEBUG,
-        "auth": {
-            "user" : config.USER_EMAIL, 
-            "pass" : config.PASSWORD_EMAIL
+        host:   config.HOST_EMAIL,
+        port:   config.PORT_EMAIL2,
+        secure: config.SECURE2,
+        debug:  config.DEBUG,
+        auth: {
+            user: config.USER_EMAIL,
+            pass: config.PASSWORD_EMAIL
         },
-        "tls" : {
-           "rejectUnauthorized" : false
-        }
+        tls: { rejectUnauthorized: false }
     });
-    
-    let info;
-    if(file==''){
-        info = await transporter.sendMail({
-            from: config.CORREO_SERVIDOR, // sender address
-            to: para, // list of receivers
-            //bcc : config.EMAIL_HIDDEN,
-            //cc : '',
-            subject: asunto, // Subject line
-            text: mensaje, // plain text body
-            html: mensaje // html body
-        },
-        function(error, info){
-            if(error){
-                return console.log(error);
-            }
-            console.log('Message sent: ' + info.response);
-        });
-    }else{
-        info = await transporter.sendMail({
-            from: config.CORREO_SERVIDOR, // sender address
-            to: para, // list of receivers
-            //bcc : config.EMAIL_HIDDEN,
-            //cc : '',
-            subject: asunto, // Subject line
-            text: mensaje, // plain text body
-            attachments: [{
-                filename: file,
-                path: ruta
-            }],
-            html: mensaje // html body
-        },
-        function(error, info){
-            if(error){
-                return console.log(error);
-            }
-            console.log('Message sent: ' + info.response);
-        });
+
+    const mailOptions = {
+        from:    config.CORREO_SERVIDOR,
+        to:      para,
+        subject: asunto,
+        text:    mensaje,
+        html:    mensaje
+    };
+
+    if (file !== '') {
+        mailOptions.attachments = [{ filename: file, path: ruta }];
     }
+
+    await transporter.sendMail(mailOptions);
 }
 
-module.exports = {
-    enviaEmail
-}
+module.exports = { enviaEmail }
