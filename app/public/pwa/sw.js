bf1ab3e9c42c90c2d4b351a57996e8f74a01ba83
Diego Alarcón <alarconpdiegoa@gmail.com>
2026-05-27 14:50:08 -0500

    se corrijio pwa

--- app/public/pwa/sw.js
+++ app/public/pwa/sw.js
@@ -1,34 +1,9 @@
-const CACHE = 'olimpo-pwa-v1';
-const OFFLINE_URL = '/instalar';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
 
-self.addEventListener('install', evt => {
-    evt.waitUntil(
-        caches.open(CACHE).then(cache => cache.add(OFFLINE_URL))
-    );
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', evt => {
-    evt.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-        )
-    );
-    self.clients.claim();
-});
-
-// Network-first: intenta red, cae a caché si falla
 self.addEventListener('fetch', evt => {
     if (evt.request.method !== 'GET') return;
-
-    if (evt.request.mode === 'navigate') {
-        evt.respondWith(
-            fetch(evt.request).catch(() => caches.match(OFFLINE_URL))
-        );
-        return;
-    }
-
     evt.respondWith(
-        fetch(evt.request).catch(() => caches.match(evt.request))
+        fetch(evt.request).catch(() => new Response('', { status: 503 }))
     );
 });
